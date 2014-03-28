1dd1b9965274627ce748606656b5b9a92ba1d103
Julia <kammerlander@ztt.fh-worms.de>
2014-03-28 15:17:06 +0100

    New Repo #1

---
+++ Tictic/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tictic
+{
+    public class Controller
+    {
+        Player player1;
+        Player player2;
+
+        public void start()
+        {
+            turn();
+        }
+
+        private void turn()
+        {
+            Console.WriteLine("Yay we started a turn! :D");
+        }
+    }
+}
---
+++ Tictic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tictic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller controller;
+            controller = new Controller();
+
+            controller.start();
+        }
+    }
+}
